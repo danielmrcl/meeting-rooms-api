07636d59229a9eece7908ce3156ca08b6920c0ee
Daniel Marcl <49128637+danielmrcl@users.noreply.github.com>
2021-04-28 20:13:22 -0300

    iniciando o projeto

---
+++ src/main/java/io/github/danielmrcl/meetingRooms/MeetingRoomsApplication.java
@@ -0,0 +1,13 @@
+package io.github.danielmrcl.meetingRooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingRoomsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/danielmrcl/meetingRooms/MeetingRoomsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.danielmrcl.meetingRooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingRoomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
