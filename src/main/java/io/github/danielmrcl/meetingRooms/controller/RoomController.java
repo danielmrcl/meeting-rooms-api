67b13ae69037fc502c2225c6f7957ef230394413
Daniel Marcl <49128637+danielmrcl@users.noreply.github.com>
2021-05-03 00:53:31 -0300

    adicionado regra para aceitar requisições do Angular

--- src/main/java/io/github/danielmrcl/meetingRooms/controller/RoomController.java
+++ src/main/java/io/github/danielmrcl/meetingRooms/controller/RoomController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/rooms")
+@CrossOrigin(origins = "http://localhost:4200")
 public class RoomController {
     @Autowired
     private RoomRepository repository;
